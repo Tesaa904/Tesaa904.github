53baa3c3c5e4187ab8283a3fa6d0da4db27a5079
Tesaa904 <86047537+Tesaa904@users.noreply.github.com>
2021-10-14 14:46:18 +0700

    Create footer.php

---
+++ layout/footer.php
@@ -0,0 +1,27 @@
+        <footer class="site-footer">
+            <div class="copy text-center">
+            &copy; copyright 2018 | by SDkanisius1murukan</a>
+              </div>
+          </div>
+      </footer>
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- CUSTOM SCRIPTS -->
+      <link rel="stylesheet" href="assets/plugins/datatables/dataTables.bootstrap.css">
+    <link href="assets/plugins/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/sweetalert.min.js"></script> <!-- lib js untuk sweet alert -->
+ <script>
+      //custom select box
+
+      $(function(){
+          $('select.styled').customSelect();
+      });
+
+  </script>
